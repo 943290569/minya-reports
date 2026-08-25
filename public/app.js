1bea08e072d7748d97d8f18c0235402fec8e4e13
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-25 22:21:26 +0300

    Replace placeholder app.js with module loader

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-PLACEHOLDER
+// Minya Landfill app loader
+[
+  "js/app-core.js",
+  "js/app-form.js",
+  "js/app-monthly.js",
+  "js/app-edit.js",
+  "js/app-print-daily.js",
+  "js/app-print-monthly.js",
+  "js/app-final.js"
+].forEach((src) => {
+  document.write(`<script src="${src}"><\/script>`);
+});
