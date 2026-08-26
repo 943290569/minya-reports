112a1380e662afa41775db1d456cce0a5060b4bf
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-26 21:49:01 +0300

    Load annual export module

--- public/app.js
+++ public/app.js
@@ -6,7 +6,8 @@
   "js/app-edit.js",
   "js/app-print-daily.js",
   "js/app-print-monthly.js",
-  "js/app-final.js"
+  "js/app-final.js",
+  "js/app-annual-export.js"
 ].forEach((src) => {
   document.write(`<script src="${src}"><\/script>`);
 });
