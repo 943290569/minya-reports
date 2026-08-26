70f2a5b6a9d384ab1ad530955d5894be6ba84681
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-26 22:13:30 +0300

    Load multi-page stylesheet

--- public/js/page-mode.js
+++ public/js/page-mode.js
@@ -3,6 +3,13 @@
 ========================================================= */
 
 (function () {
+  if (!document.querySelector('link[href="multipage.css"]')) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "multipage.css";
+    document.head.appendChild(link);
+  }
+
   function getPageFromPath() {
     const path = window.location.pathname.replace(/\/+$/, "") || "/";
     if (path === "/report") return "report";
