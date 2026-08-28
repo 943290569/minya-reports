d276c50eef510a7b3ba73a22e13cc6f2cbf75213
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-28 21:05:16 +0300

    Refresh visible header logout asset

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Minya Landfill app loader
-const MINYA_ASSET_VERSION = "3.2.0-logout-header-v2";
+const MINYA_ASSET_VERSION = "3.2.0-logout-header-v3";
 const MINYA_LOADING_STARTED_AT = Date.now();
 const MINYA_LOADING_MIN_MS = 150;
 
