391e20c598b12c296db2eee8fe552305537fe3c4
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-28 22:16:23 +0300

    Bump asset version after removing account display

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Minya Landfill app loader
-const MINYA_ASSET_VERSION = "3.2.0-account-fixed-v1";
+const MINYA_ASSET_VERSION = "3.2.0-account-hidden-v1";
 const MINYA_LOADING_STARTED_AT = Date.now();
 const MINYA_LOADING_MIN_MS = 150;
 
