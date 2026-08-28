e0fc6e4a15c6907fb9200cd3a63316cf8c960ffc
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-28 21:32:55 +0300

    Load full-site review print from dashboard

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Minya Landfill app loader
-const MINYA_ASSET_VERSION = "3.2.0-review-print-v2";
+const MINYA_ASSET_VERSION = "3.2.0-site-review-print-v1";
 const MINYA_LOADING_STARTED_AT = Date.now();
 const MINYA_LOADING_MIN_MS = 150;
 
@@ -107,7 +107,8 @@ const MINYA_LOADING_MIN_MS = 150;
   "js/app-report-workflow.js",
   "js/app-reviews.js",
   "js/app-logout-header.js",
-  "js/app-review-print.js"
+  "js/app-review-print.js",
+  "js/app-site-review-print.js"
 ].forEach((src) => {
   const versionedSrc = `${src}?v=${MINYA_ASSET_VERSION}`;
   document.write(`<script src="${versionedSrc}"><\/script>`);
