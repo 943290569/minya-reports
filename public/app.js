a9c4e94f32f58c85bfe1f9e908d4029e95fffc1b
943290569 <eng.mohamad.jeb@gmail.com>
2026-08-29 19:40:57 +0300

    Bump frontend asset version after annual and Drive fixes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Minya Landfill app loader
-const MINYA_ASSET_VERSION = "3.3.0-drive-import-v1";
+const MINYA_ASSET_VERSION = "3.3.0-20260829-v2";
 const MINYA_LOADING_STARTED_AT = Date.now();
 const MINYA_LOADING_MIN_MS = 150;
 
