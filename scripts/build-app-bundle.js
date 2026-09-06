488a3c21f4892a99cc0948af2ac9e576b95e2bcd
943290569 <eng.mohamad.jeb@gmail.com>
2026-09-06 06:19:43 +0300

    ربط تنبيه التقرير المعاد بالحزمة

--- scripts/build-app-bundle.js
+++ scripts/build-app-bundle.js
@@ -26,7 +26,7 @@ const versionMatch = loader.match(/const\s+MINYA_ASSET_VERSION\s*=\s*["']([^"']+
 if (!versionMatch) {
   throw new Error("MINYA_ASSET_VERSION is missing from public/app.js");
 }
-const assetVersion = `${versionMatch[1]}-wa1`;
+const assetVersion = `${versionMatch[1]}-wa2`;
 
 const loaderBlock = loader.slice(start, end + endMarker.length);
 const styleBlock = loader.slice(styleStart, styleEnd + styleEndMarker.length);
@@ -45,6 +45,11 @@ const adminWhatsAppModule = "js/app-admin-whatsapp.js";
 if (!modulePaths.includes(adminWhatsAppModule)) {
   const adminUsersIndex = modulePaths.indexOf("js/app-admin-users.js");
   modulePaths.splice(adminUsersIndex >= 0 ? adminUsersIndex + 1 : modulePaths.length, 0, adminWhatsAppModule);
+}
+const returnedReportModule = "js/app-returned-report-notice.js";
+if (!modulePaths.includes(returnedReportModule)) {
+  const workflowIndex = modulePaths.indexOf("js/app-report-workflow.js");
+  modulePaths.splice(workflowIndex >= 0 ? workflowIndex + 1 : modulePaths.length, 0, returnedReportModule);
 }
 const stylePaths = [...styleBlock.matchAll(/"([^"]+\.css)"/g)].map((match) => match[1]);
 
