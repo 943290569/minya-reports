6d4b6641b11814802abedaf67798ed5ea29d7372
943290569 <eng.mohamad.jeb@gmail.com>
2026-09-07 20:58:10 +0300

    Refresh PWA cache after moving notification control

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='minya-pwa-v2';
+const CACHE_NAME='minya-pwa-v3';
 const STATIC_ASSETS=['/','/style.css','/manifest.webmanifest','/assets/app-icon.svg','/assets/app-icon-maskable.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
