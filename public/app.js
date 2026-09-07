fdbd5f7cbee8d54fd6d9efab529c00f15aa56566
943290569 <eng.mohamad.jeb@gmail.com>
2026-09-07 10:02:10 +0300

    Release complete admin freeze fix

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 // Minya Landfill app loader
-const MINYA_ASSET_VERSION = "3.5.0-20260907-stable6";
+const MINYA_ASSET_VERSION = "3.5.0-20260907-stable7";
 const MINYA_LOADING_STARTED_AT = Date.now();
 const MINYA_APPEARANCE_STORAGE_KEY = "minya_appearance_settings_v1";
 
