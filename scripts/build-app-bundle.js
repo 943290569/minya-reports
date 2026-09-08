cfa0b491cc4dc93ecada6b893210afc47f2d323f
943290569 <eng.mohamad.jeb@gmail.com>
2026-09-08 06:36:39 +0300

    Bundle pre-replace import backup guard

--- scripts/build-app-bundle.js
+++ scripts/build-app-bundle.js
@@ -30,7 +30,7 @@ if (!versionMatch) {
   throw new Error("MINYA_ASSET_VERSION is missing from public/app.js");
 }
 const assetVersion = `${versionMatch[1]}-wa8`;
-const driveAssetVersion = `${assetVersion}-drive2`;
+const driveAssetVersion = `${assetVersion}-drive3`;
 
 const driveScriptPaths = [
   "js/app-auth.js",
@@ -48,6 +48,7 @@ const driveScriptPaths = [
   "js/app-drive-quality-details.js",
   "js/app-drive-preimport-consistency.js",
   "js/app-drive-postimport-verify.js",
+  "js/app-drive-pre-replace-backup.js",
   "js/app-drive-folder-nav.js",
   "js/app-english-digits.js"
 ];
