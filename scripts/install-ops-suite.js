6c57975b5cdc02ddc2ac568f329686232d9480eb
943290569 <eng.mohamad.jeb@gmail.com>
2026-09-12 09:58:09 +0300

    Deploy startup performance update

--- scripts/install-ops-suite.js
+++ scripts/install-ops-suite.js
@@ -35,6 +35,6 @@ if(backupPattern.test(server)){
 fs.writeFileSync(serverPath,server,'utf8');
 require('./install-monthly-close');
 let app=fs.readFileSync(appPath,'utf8');
-app=app.replace(/const MINYA_ASSET_VERSION = "[^"]+";/,'const MINYA_ASSET_VERSION = "3.5.0-20260912-annual-comparison-v2";');
+app=app.replace(/const MINYA_ASSET_VERSION = "[^"]+";/,'const MINYA_ASSET_VERSION = "3.5.0-20260912-startup-speed-v1";');
 fs.writeFileSync(appPath,app,'utf8');
 console.log('Operations suite routes, permissions, complete backup/restore limits and asset version installed.');
